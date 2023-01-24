ce9025b7d7ee13840754e259d0efcc3933587f8f
JM <jacobmontpetit@gmail.com>
2023-01-23 22:09:19 -0500

    system:main: lab1 Section C run your first program

--- system/main.c
+++ system/main.c
@@ -3,21 +3,9 @@
 #include <xinu.h>
 #include <stdio.h>
 
-int main(int argc, char **argv)
+void main(int argc, char **argv)
 {
-	uint32 retval;
 
-	resume(create(shell, 8192, 50, "shell", 1, CONSOLE));
-
-	/* Wait for shell to exit and recreate it */
-
-	recvclr();
-	while (TRUE) {
-		retval = receive();
-		kprintf("\n\n\rMain process recreating shell\n\n\r");
-		resume(create(shell, 4096, 1, "shell", 1, CONSOLE));
-	}
-	while (1);
-
-	return OK;
+	printf("Hello, world!!!!\n");
 }
+
