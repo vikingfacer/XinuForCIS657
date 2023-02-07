caec9f84cb5761d6d29a74937822728a8054cdba
JM <jacobmontpetit@gmail.com>
2023-02-06 21:54:08 -0500

    signaler & waiter

--- system/main.c
+++ system/main.c
@@ -1,23 +1,39 @@
-/*  main.c  - main */
-
 #include <xinu.h>
 #include <stdio.h>
 
-int main(int argc, char **argv)
+void signaler(void);
+void waiter(void);
+sid32 sem;
+pid32 spid, wpid;
+
+void main(void)
 {
-	uint32 retval;
-
-	resume(create(shell, 8192, 50, "shell", 1, CONSOLE));
-
-	/* Wait for shell to exit and recreate it */
-
-	recvclr();
-	while (TRUE) {
-		retval = receive();
-		kprintf("\n\n\rMain process recreating shell\n\n\r");
-		resume(create(shell, 4096, 1, "shell", 1, CONSOLE));
-	}
-	while (1);
-
+	sem = semcreate(20);
+	wpid = create(waiter, 1024, 40, "waiter", 0);
+	resume(wpid);
+	spid = create(signaler, 1024, 20, "signaler", 0);
+	resume(spid);
 	return OK;
+}
+
+void signaler(void)
+{
+	while(1)
+	{
+		printf("\nSignaler is running \n");
+		int32 i;
+		signaln(sem,5);
+	}
+}
+
+void waiter(void)
+{
+	int32 i;
+	for (i = 1; i <= 2000; i++)
+	{
+		printf("%d ", i);
+		wait(sem);
+	}
+	kill(spid);
+	kill(wpid);
 }
