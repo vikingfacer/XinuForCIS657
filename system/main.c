c9374d2089bf815e31ea8010c5a683c47082cffa
JM <jacobmontpetit@gmail.com>
2023-02-07 19:51:10 -0500

    system:main: Add queuetab dump

--- system/main.c
+++ system/main.c
@@ -1,23 +1,27 @@
-/*  main.c  - main */
-
 #include <xinu.h>
 #include <stdio.h>
 
-int main(int argc, char **argv)
+void infLoop();
+
+void main(void)
 {
-	uint32 retval;
-
-	resume(create(shell, 8192, 50, "shell", 1, CONSOLE));
-
-	/* Wait for shell to exit and recreate it */
-
-	recvclr();
-	while (TRUE) {
-		retval = receive();
-		kprintf("\n\n\rMain process recreating shell\n\n\r");
-		resume(create(shell, 4096, 1, "shell", 1, CONSOLE));
+	int n = 1, nthr = 10; //nthr is number of threads to execute
+	
+	for (; n <= nthr; n++)
+	{
+		resume(create(infLoop, 1024, 20, "proc %d", n, 1, CONSOLE));
 	}
-	while (1);
+	
+	printf("|  ID  |  key  |  prev  |  next  |\n");
+	printf("|------|-------|--------|--------|\n");
+	int i = 0;
+	for (; i < NPROC && queuetab[i-1].qnext != NULL; i++)
+	{
+		printf("| %4d |  %3d  |  %4d  |  %4d  |\n",i, queuetab[i].qkey, queuetab[i].qprev, queuetab[i].qnext);
+	}
+}
 
-	return OK;
+void infLoop()
+{
+	while (1) ;
 }
