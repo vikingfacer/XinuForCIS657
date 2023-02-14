bab91fdbbaa5c40590c681152c43a59070d4c088
JM <jacobmontpetit@gmail.com>
2023-02-13 19:47:08 -0500

    lab3part2 starting point

--- system/main.c
+++ system/main.c
@@ -1,23 +1,55 @@
-/*  main.c  - main */
-
+/*
+ * Modify the producer-consumer code in file ex5.c
+ * (in the book and lecture not in xinu directory, see below)
+ * to use a buffer of 15 slots, and have the producer and consumer synchronize
+ * in such a way that the producer can generate up to 15 values
+ * before blocking and a consumer can extract all values in the buffer before blocking.
+ * That is, arrange for the producer to write 1, 2, ... in successive locations of the buffer,
+ * wrapping around to the beginning after filling the last slot,
+ * and have the consumer extract values and print them on the console.
+ * How many semaphores are needed?
+ */
 #include <xinu.h>
-#include <stdio.h>
-
-int main(int argc, char **argv)
+void prod2(sid32, sid32);
+void cons2(sid32, sid32);
+int32 n = 0; /* Variable n has inital value zero */
+/*-------------------------------------------------------------------
+* main - Producer and Consumer processes synchronized with semaphores
+*--------------------------------------------------------------------
+*/
+void main(void)
 {
-	uint32 retval;
-
-	resume(create(shell, 8192, 50, "shell", 1, CONSOLE));
-
-	/* Wait for shell to exit and recreate it */
-
-	recvclr();
-	while (TRUE) {
-		retval = receive();
-		kprintf("\n\n\rMain process recreating shell\n\n\r");
-		resume(create(shell, 4096, 1, "shell", 1, CONSOLE));
+	sid32 produced, consumed;
+	consumed = semcreate(0);
+	produced = semcreate(1);
+	resume( create(cons2, 1024, 20, "cons", 2, consumed, produced) );
+	resume( create(prod2, 1024, 20, "prod", 2, consumed, produced) );
+}
+/*-------------------------------------------------------------------------
+* prod2 - Increment n 2000 times, waiting for it to be consumed
+*---------------------------------------------------------------------------
+*/
+void prod2(sid32 consumed, sid32 produced)
+{
+	int32 i;
+	for(i=1; i<=2000; i++)
+	{
+		wait(consumed);
+		n++;
+		signal(produced);
 	}
-	while (1);
-
-	return OK;
+}
+/*-------------------------------------------------------------------------
+* cons2 - Print n 2000 times, waiting for it to be produced
+*---------------------------------------------------------------------------
+*/
+void cons2(sid32 consumed, sid32 produced)
+{
+	int32 i;
+	for(i=1; i<=2000; i++)
+	{
+		wait(produced);
+		printf("n is %d \n", n);
+		signal(consumed);
+	}
 }
